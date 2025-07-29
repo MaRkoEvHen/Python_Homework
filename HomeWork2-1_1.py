db094ff42b586870a7f01d199d0cc34ad634f394
MaRkoEvHen <marik27102003t@gmail.com>
2025-07-29 05:26:34 +0300

    HomeWork2-1_2

--- HomeWork2-1_1.py
+++ HomeWork2-1_1.py
@@ -1,8 +1,8 @@
 get_number = int(input("Введіть 4-х значне число:"))
-num1= get_number//1000
-num2= (get_number%1000)//100
-num3= (get_number%100)//10
-num4= get_number%10
+num1= get_number//1000 #ділимо тисячну частину націло
+num2= (get_number%1000)//100 #ділимо соту частину спочатку в дужках потім остачу на 100
+num3= (get_number%100)//10 #ділимо десяту частину спочатку в дужках потім остачу на 10
+num4= get_number%10 #ділимо залишок на 10
 print(num1)
 print(num2)
 print(num3)
