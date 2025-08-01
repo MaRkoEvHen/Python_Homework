ea5919cb925b6670f45b195cdfbf88af9dbf8d38
MaRkoEvHen <marik27102003t@gmail.com>
2025-08-02 00:11:53 +0300

    HomeWork3-1

---
+++ HomeWork3-2.py
@@ -0,0 +1,11 @@
+type_list = [1,2,3,4,5,6,7,8,9]
+if len(type_list) <= 1:
+    print(f"Ми його не змінюємо {type_list}")
+else:
+    a = type_list[-1]
+    del type_list[-1]
+    type_list.insert(0, a)
+    print(type_list)
+
+
+
