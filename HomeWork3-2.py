88c6ca32750dd8e9d5e5a3c00bbce85365e70da2
MaRkoEvHen <marik27102003t@gmail.com>
2025-08-02 00:16:07 +0300

    HomeWork3-1

--- HomeWork3-2.py
+++ HomeWork3-2.py
@@ -1,11 +1,11 @@
 my_list = [1,2,3,4,5,6,7,8,9]
-if len(type_list) <= 1:
-    print(f"Ми його не змінюємо {type_list}")
+if len(my_list) <= 1:
+    print(f"Ми його не змінюємо {my_list}")
 else:
-    a = type_list[-1]
-    del type_list[-1]
-    type_list.insert(0, a)
-    print(type_list)
+    a = my_list[-1]
+    del my_list[-1]
+    my_list.insert(0, a)
+    print(my_list)
 
 
 
