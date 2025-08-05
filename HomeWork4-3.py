3501eedd9da5af0a0f4a2babce872751ef6f2149
MaRkoEvHen <marik27102003t@gmail.com>
2025-08-05 14:07:12 +0300

    HomeWork4-3

--- HomeWork4-3.py
+++ HomeWork4-3.py
@@ -5,5 +5,5 @@ list_length = random.randint(3, 10)
 for el in range(list_length):
     my_list.append(random.randint(3,10))
 new_list=[my_list[0],my_list[2],my_list[-2]]
-print(new_list)
-print(my_list)
+print(my_list) #Вивід згенерованого нового списку
+print(new_list) #Вивід нового списку з 3-ма елементами
