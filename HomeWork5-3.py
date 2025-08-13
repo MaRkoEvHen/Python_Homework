cde25786e353e9c0f67b6b131dd48e3fd353f2e5
MaRkoEvHen <marik27102003t@gmail.com>
2025-08-13 22:07:19 +0300

    HomeWork5-3

--- HomeWork5-3.py
+++ HomeWork5-3.py
@@ -3,9 +3,10 @@ get_string= input("Введіть рядок: ")
 for char in string.punctuation:
     get_string= get_string.replace(char, " ")
 words = get_string.split()
+hashtag = "#"
 for word in words:
-    word.capitalize()
-hashtag = "#" + "".join(word.capitalize() for word in words)
+    cap_word=word.capitalize()
+    hashtag += cap_word
 if len(hashtag) > 140:
     print("Хештег занадто довгий")
 print(hashtag)
