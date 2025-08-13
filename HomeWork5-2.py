aa2ff44479fffa44372927e9ec28fe7c6a82f052
MaRkoEvHen <marik27102003t@gmail.com>
2025-08-13 21:26:19 +0300

    HomeWork5-2

---
+++ HomeWork5-2.py
@@ -0,0 +1,37 @@
+while True:
+    get_number1 = input("Введіть число 1-ше число: ")
+    get_action = input("Введіть дію +,-,*,/,%,//,**: ")
+    get_number2 = input("Введіть число 2-ше число: ")
+
+    get_number1 = int(get_number1)
+    get_number2 = int(get_number2)
+
+
+    if get_number2 == 0 and get_action in ("/", "//", "%"):  # перевірка ділення на нуль
+        print("На нуль ділити не можна")
+    elif get_action == "+":
+        summa = get_number1 + get_number2
+        print(f"{get_number1} + {get_number2} = {summa} ")
+    elif get_action == "-":
+        minus = get_number1 - get_number2
+        print(f"{get_number1} - {get_number2} = {minus} ")
+    elif get_action == "*":
+        multiplication = get_number1 * get_number2
+        print(f"{get_number1} * {get_number2} = {multiplication} ")
+    elif get_action == "/":
+        dilFloat = get_number1 / get_number2
+        print(f"{get_number1} / {get_number2} = {dilFloat} ")
+    elif get_action == "//":
+        dilInt = get_number1 // get_number2
+        print(f"{get_number1} // {get_number2} = {dilInt} ")
+    elif get_action == "%":
+        fraction = get_number1 % get_number2
+        print(f"{get_number1} % {get_number2} = {fraction} ")
+    elif get_action == "**":
+        step = get_number1 ** get_number2
+        print(f"{get_number1} ** {get_number2} = {step} ")
+    else:
+        print("Ви ввели не правильну дію")
+    if input("Бажаєте продовжити? (так/ні): ").lower() != "так":
+        print("Допобачення.")
+        break
