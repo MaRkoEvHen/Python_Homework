e03fe1c0a699ec87a22c78e9fb509243e5bcb23f
MaRkoEvHen <marik27102003t@gmail.com>
2025-08-16 22:11:52 +0300

    HomeWork6-1

--- HomeWork6-1.py
+++ HomeWork6-1.py
@@ -2,7 +2,7 @@ import string
 
 range_of_letters = input(f"Enter a range of letters (a-c), (a-H): ").strip()
 letters = range_of_letters.split('-')
-letter_one= letters[0].strip()
+letter_one = letters[0].strip()
 letter_two = letters[1].strip()
 index_one = string.ascii_letters.index(letter_one)
 index_two = string.ascii_letters.index(letter_two)
